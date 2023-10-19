d8d306f194c5dc7d275588e90e9cd23f2b0c26c3
Gonça <148438782+GoncaloPereira97@users.noreply.github.com>
2023-10-19 12:26:30 +0100

    Fix Ex_01

--- FichasJava/src/FichaPratica01/Ex_01.java
+++ FichasJava/src/FichaPratica01/Ex_01.java
@@ -1,4 +1,28 @@
 package FichaPratica01;
 
+import java.util.Scanner;
+
 public class Ex_01 {
+    public static void main(String[] args) {
+
+        // Instanciar o Scanner (obrigatório para todos os exercicios)
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variaveis
+        int num1, num2, soma;
+
+        // ler num1
+        System.out.print("Insira um número: ");
+        num1= input.nextInt();
+
+        // ler num2
+        System.out.print("Insira um número: ");
+        num2= input.nextInt();
+
+        // Calcular a soma de num1+num2
+        soma= num1 + num2;
+
+        // Apresentar a soma
+        System.out.print("A soma é: " + soma);
+    }
 }
