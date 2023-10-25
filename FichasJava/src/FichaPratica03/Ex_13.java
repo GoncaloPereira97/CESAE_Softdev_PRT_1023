74cfe14637e8a3da4dbe2fd032b1537c72d1d813
Gonça <148438782+GoncaloPereira97@users.noreply.github.com>
2023-10-25 09:07:52 +0100

    update?

--- FichasJava/src/FichaPratica03/Ex_13.java
+++ FichasJava/src/FichaPratica03/Ex_13.java
@@ -6,21 +6,21 @@ public class Ex_13 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int quant, num = 0, num1, num2;
-
-
-        System.out.print("Quantos números deseja inserir: ");
-        quant = input.nextInt();
-
-
-        while (num < quant) {
-            num = num + 1;
-
-            System.out.print("Introduza um número: ");
-            num1 = input.nextInt();
-            if (num2 < num1) {
-                num2 = num1 + num2;
-            } 
+//        int quant, num = 0, num1, num2;
+//
+//
+//        System.out.print("Quantos números deseja inserir: ");
+//        quant = input.nextInt();
+//
+//
+//        while (num < quant) {
+//            num = num + 1;
+//
+//            System.out.print("Introduza um número: ");
+//            num1 = input.nextInt();
+//            if (num2 < num1) {
+//                num2 = num1 + num2;
+//            }
 
         }
 }
