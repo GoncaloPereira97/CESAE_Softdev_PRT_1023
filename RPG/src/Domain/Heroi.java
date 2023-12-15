d3affc16af8b7e340414fa8f0337370b5b14837d
GoncaloPereira97 <148438782+GoncaloPereira97@users.noreply.github.com>
2023-12-15 15:49:07 +0000

    inicio projeto RPG

---
+++ RPG/src/Domain/Heroi.java
@@ -0,0 +1,24 @@
+package Domain;
+
+import Controllers.AtackStrategy;
+import Controllers.CaracterCreation;
+
+public class Heroi extends Entidade{
+
+    private static Heroi instance;
+
+    private AtackStrategy classType;
+
+    private Heroi(String nome, int maxHP, int HP, int strength, int inteligence, int dexterity, int level, int gold, Item primaryWeapon, AtackStrategy classType) {
+        super(nome, maxHP, HP, strength, inteligence, dexterity, level, gold, primaryWeapon);
+        this.classType = classType;
+    }
+
+    public static Heroi getInstance() {
+        if (instance == null){
+        }
+
+
+        return instance;
+    }
+}
